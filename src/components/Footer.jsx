3a06fbc0178348ec8396a2c767f021a68a5c62ad
MatthewDelong <matthew.delong@hotmail.co.uk>
2025-08-13 12:05:16 +0100

    amended footer links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 // src/components/Footer.jsx
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
   return (
@@ -9,35 +9,49 @@ const Footer = () => {
           <div className="text-sm sm:text-base">
             © 2025 Social-App. All rights reserved.
           </div>
-          <div className="text-sm text-gray-300">
-            MIT License
-          </div>
-          
+
           <div className="text-xs">
-            <span className="small">
-            Site content licensed under{" "}
-            <a 
-              href="https://creativecommons.org/licenses/by/4.0/" 
-              target="_blank" 
+            <a
+              href="https://github.com/MatthewDelong/README.md/blob/main/LICENSE"
+              target="_blank"
               rel="noopener noreferrer"
+              class="text-white text-decoration-none"
               className="text-blue-300 hover:text-blue-200 hover:underline transition-colors duration-200"
             >
-              CC BY 4.0
+              MIT License
             </a>
-            <a 
-              href="https://creativecommons.org/licenses/by/4.0/" target="_blank" 
-              rel="noopener noreferrer"
-               class="text-white ms-1">
+          </div>
 
-        <img src="https://mirrors.creativecommons.org/presskit/icons/cc.svg" 
-             alt="Creative Commons" 
-             className="h-4 aligh-text-top inline-block" />
+          <div className="text-xs">
+            <span className="small">
+              Site content licensed under{" "}
+              <a
+                href="https://creativecommons.org/licenses/by/4.0/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-blue-300 hover:text-blue-200 hover:underline transition-colors duration-200"
+              >
+                CC BY 4.0
+              </a>
+              <a
+                href="https://creativecommons.org/licenses/by/4.0/"
+                target="_blank"
+                rel="noopener noreferrer"
+                class="text-white ms-1"
+              >
+                <img
+                  src="https://mirrors.creativecommons.org/presskit/icons/cc.svg"
+                  alt="Creative Commons"
+                  className="h-4 aligh-text-top inline-block"
+                />
 
-        <img src="https://mirrors.creativecommons.org/presskit/icons/by.svg" 
-             alt="Attribution" 
-             className="h-4 aligh-text-top inline-block" />
-    </a>
-    </span>
+                <img
+                  src="https://mirrors.creativecommons.org/presskit/icons/by.svg"
+                  alt="Attribution"
+                  className="h-4 aligh-text-top inline-block"
+                />
+              </a>
+            </span>
           </div>
         </div>
       </div>
